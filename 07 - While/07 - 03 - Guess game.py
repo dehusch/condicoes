f592f329cd545082e93ba37ee1cc514d6724e5a3
Denis.Husch <denis.husch@gmail.com>
2021-05-04 22:28:59 -0300

    Restart py learning

--- 07 - While/07 - 03 - Guess game.py
+++ 07 - While/07 - 03 - Guess game.py
@@ -13,3 +13,18 @@ while num != jogador:
         print('É um numero maior!')
 print('Parabens, você acertou!')
 print(f'Voce acentrou com {palpites} palpites')
+
+#Outra forma
+
+import random
+numeroc = (random.randint(1, 10))
+numerou = 0
+chance = 0
+while numerou != numeroc:
+  numerou = int(input('Digite um numero: '))
+  chance += 1
+  if numerou < numeroc:
+    print('O numero é mais alto')
+  elif numerou > numeroc:
+    print('O numero é menor')
+print(f'Acertou com {chance} palpites')
--- 07 - While/07 - 04 - Menu.py
+++ 07 - While/07 - 04 - Menu.py
@@ -27,3 +27,29 @@ while opcao != 5:
     elif opcao == 5:
         print('Finalizando!!!')
 print('Fim do programa')
+
+#novo formato
+
+number1 = int(input('Digite um valor: '))
+number2 = int(input('Digite outro valor: '))
+opcao1 = 0
+while opcao1 != 5:
+  print('[1] SOMAR')
+  print('[2] MULTIPLICAR')
+  print('[3] MAIOR NUMERO')
+  print('[4] NOVOS NUMEROS')
+  print('[5] FINALIZAR PROGRAMA')
+  opcao1 = int(input('Escolha uma opção: '))
+  if opcao1 == 1:
+    print(f' A soma é {number1 + number2}')
+  elif opcao1 == 2:
+    print(f'A multiplicação é {number1 * number2}')
+  elif opcao1 == 3:
+    if number1 > number2:
+      print(f'{number1} é o maior')
+    else:
+      print(f'O numero maior é {number2}')
+  elif opcao1 == 4:
+    number1 = int(input('Digite um valor: '))
+    number2 = int(input('Digite outro valor: '))
+print('FIM')
