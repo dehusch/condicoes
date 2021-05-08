07d10b8a3a95c31e768c7d2c11f31f3ad489a859
Denis.Husch <denis.husch@gmail.com>
2021-05-07 23:49:07 -0300

    Restart py learning

--- 07 - While/07 - 07 -Sequencia de fibonacci.py
+++ 07 - While/07 - 07 -Sequencia de fibonacci.py
@@ -1,15 +1,17 @@
 #sequencia de fibonacci
 #sequencia de fibonacci é 0 - 1 -1 - 2 - 3 - 5 - 8
 #0 + 1 = 1, 1 + 1 = 2, 1 + 2 = 3, 3 + 2 = 5, 5 + 3 = 8
+#To assume the next value, we have to change the t1 and t2 position
+#cont = 3 'cause for 3 results it's not necessary the loop
 n = int(input('Quantas sequencias você quer?: '))
 t1 = 0
 t2 = 1
-print(f'{t1} > {t2}', end='')
+print(f'{t1} > {t2}', end=' ')
 cont = 3
 while cont <= n:
     t3 = t1 + t2
-    print(f' > {t3}', end='')
-    t1 = t2
-    t2 = t3
+    print(f' > {t3}', end=' ')
+    t1 = t2 #t1 assumindo o valor de t2
+    t2 = t3 #t2 assumindo o valor de t3
     cont = cont + 1
 print('Fim')
