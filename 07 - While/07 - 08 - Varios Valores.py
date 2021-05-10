e2bb4c89ad3609f5a97f285322ddb30434f3a293
Denis.Husch <denis.husch@gmail.com>
2021-05-10 00:00:28 -0300

    Soma de varios valores

--- 07 - While/07 - 08 - Varios Valores.py
+++ 07 - While/07 - 08 - Varios Valores.py
@@ -7,3 +7,15 @@ while num != 999:
     num = int(input('Digite um valor para ser somado: ')) # o Num repete no comeco para
 #desconsiderar o 999 na soma
 print(f'Voce digitou {cont} numeros e a soma deles é {soma}')
+
+#outra forma
+
+num = 0
+contador = 0
+soma = 0
+num = int(input('Digite um valor [999 para cancelar]: '))
+while num != 999:
+  soma = soma + num
+  contador = contador + 1
+  num = int(input('Digite um valor [999 para cancelar]: '))
+print(f'Voce digitou {contador} numeros e a soma é {soma}')
