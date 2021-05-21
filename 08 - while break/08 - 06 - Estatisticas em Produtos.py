6823cbbdd19a59dc06e7f1531655804daa8d457a
Guilherme Borges <guilhermeborges84@gmail.com>
2021-05-20 23:03:00 -0300

    atualizado menor produto

--- 08 - while break/08 - 06 - Estatisticas em Produtos.py
+++ 08 - while break/08 - 06 - Estatisticas em Produtos.py
@@ -8,6 +8,7 @@ produtos = 0
 preco = 0
 barato = " "
 contprod = 0
+primeiravez= 's'
 while True:
     produtos = str(input("Digite o nome do produto: "))
     preco = int(input("Qual o valor do produto?: "))
@@ -15,9 +16,23 @@ while True:
     if preco > 1000:
         contprod = contprod + 1
     cont = " "
+    if primeiravez == 's':
+        # Definindo o nome do menor produto
+        menorProduto = produtos
+        # Definindo o valor do menor produto
+        menorPreco = preco
+        primeiravez = 'n'
+    elif menorProduto > produtos:
+        menorProduto = produtos
+        menorPreco = preco
+
     while cont not in "SN":
         cont = str(input("Deseja continuar? [S/N]: ")).upper()
     if cont == "N":
+
         break
-print(f"Total de produtos acima de {contprod}")
+print(f"Total de produtos acima de 1000 é igual a: {contprod}")
 print(f"Preco total: {totalcompra}")
+print(f"O menor produto é: {menorProduto} ")
+print(f"O valor do menor produto é: {menorPreco} ")
+
