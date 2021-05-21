4ca445b25144466d8e34c95caa4bf542e26b935b
Denis.Husch <denis.husch@gmail.com>
2021-05-21 01:12:05 -0300

    While break exercise

--- 08 - while break/08 - 07 - Caixa eletronico.py
+++ 08 - while break/08 - 07 - Caixa eletronico.py
@@ -16,10 +16,24 @@ print(f"Notas de 1: {nota1}")
 #
 #Outra Forma
 #
-saque = int(input("Digite o valor para sacar: "))
-saida = 0
-
-while saida < saque :
-    saidanota50 = nota
-
-
+val1 = int(input("Digite o valor para sacar: "))
+total = val1
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total = total - ced
+        totced = totced + 1
+    else:
+        if totced > 0:
+            print(f"Total de {totced} cedulas de R${ced}")
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print("Fim do programa")
