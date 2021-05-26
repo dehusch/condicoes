c8e750e766dff7e26752fb0767977a15018fddca
Guilherme Borges <guilhermeborges84@gmail.com>
2021-05-25 23:39:35 -0300

    Insert while onde verifica se o número está entre zero e dez

--- 09 - Tuple/09 - 02 - Contagem Extenso.py
+++ 09 - Tuple/09 - 02 - Contagem Extenso.py
@@ -1,7 +1,7 @@
 numero = ("Zero", "Um", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez")
 escolha = int(input("Digite um numero entre O e 10:"))
 while escolha > 10 or escolha < 0:
-    print("Digite novamente")
+    print(f'Você digitou errado digite um número valido')
     escolha = int(input("Digite um numero entre O e 10:"))
-    print(numero[escolha])
-print("FIM")
+
+print(numero[escolha])
