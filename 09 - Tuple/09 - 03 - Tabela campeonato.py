090846890fe35a0bbbd9751a6ab6efc5a15a8ae9
denis <denis.husch@gmail.com>
2021-05-27 22:55:27 -0300

    Tuples Soccer Time

--- 09 - Tuple/09 - 03 - Tabela campeonato.py
+++ 09 - Tuple/09 - 03 - Tabela campeonato.py
@@ -1,1 +1,7 @@
-time = ("América-MG", "Athletico", "Atlético-GO", "Atlético-MG", "Bahia", "Ceará", "Chapecoense")
+time = ("América-MG", "Sao Paulo", "Athletico", "Atlético-GO", "Atlético-MG", "Bahia", "Ceará", "Chapecoense")
+print(f'Estes são os times da tabela do Campeonato Brasileiro: ')
+print(time)
+print(f'Estes são os 5 primeiros: {time[0: 5]} ')
+print(f'Os ultimos quatro colocados: {time[-4:]}')
+print(f'Classificação em ordem alfabetica: {sorted(time)}')
+print(time.index("Chapecoense"))
