8f2701c7833e8a2d64081d825752062cfb0af658
Guilherme Borges <guilhermeborges84@gmail.com>
2021-05-27 22:44:35 -0300

    Adicionado os 5 primeiros

--- 09 - Tuple/09 - 04 - Tabela Campeonato.py
+++ 09 - Tuple/09 - 04 - Tabela Campeonato.py
@@ -1,1 +1,18 @@
-times = ("")
+time = ("América-MG", "Athletico", "Atlético-GO", "Atlético-MG", "Bahia", "Ceará", "Chapecoense")
+
+'''os ultimos 4 colocados
+ordem alfabética
+em que posicao está x time
+'''
+
+#quantidade = int(input('Digite a quantidade de posições que deseja saber: '))
+quantidade = 5
+#Nesse for percorre em times a quantidade de times digitada
+for primeiros in range(0,quantidade):
+    print( time[primeiros])
+    print(' ----------------------')
+
+for ultimos in range(0,len(time)):
+    print( time[(len(time))- ultimos])
+
+
