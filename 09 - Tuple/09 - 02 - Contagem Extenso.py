6dee07ea85936e8fceda3386743119e61e6b78de
Denis.Husch <denis.husch@gmail.com>
2021-08-07 17:45:30 -0300

    Tuple Exerc. 2

--- 09 - Tuple/09 - 02 - Contagem Extenso.py
+++ 09 - Tuple/09 - 02 - Contagem Extenso.py
@@ -5,3 +5,11 @@ while escolha > 10 or escolha < 0:
     escolha = int(input("Digite um numero entre O e 10:"))
 print(numero[escolha])
 print("FIM")
+#outro metodo
+cont = ("Zero", "Um", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez")
+while True:
+    num = int(input('Digite um numero entre 0 e 10: '))
+    if 0 <= num <= 10:
+        break
+    print('Tente novamente!\n')
+print(f'Voce digitou o numero {cont[num]}')
