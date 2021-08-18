35b71241f024382bbaabb960bfcbb4ea6c524da3
Denis.Husch <denis.husch@gmail.com>
2021-08-17 23:21:15 -0300

    Exerc 6

--- 11 - Dictionary/11 - 07 - Dict improve.py
+++ 11 - Dictionary/11 - 07 - Dict improve.py
@@ -2,22 +2,46 @@
 #Ask the player's name and how many games played with his score
 #write an option to include N players
 #write an option to show players details
+time = list()
 jogador = dict()
 partidas = list()
-jogador['nome'] = str(input('Nome do jogador: '))
-jogos = int(input('Quantas partidas jogou?: '))
-for c in range(0, jogos):
-    partidas.append(int(input(f'Quanto gols na partida {c+1}?: ')))
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(partidas)
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    jogos = int(input('Quantas partidas jogou?: '))
+    partidas.clear()
+    for c in range(0, jogos):
+        partidas.append(int(input(f'Quanto gols na partida {c+1}?: ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        print('S ou N')
+    if resp == "N":
+        break
 print()
-print(jogador)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
 print()
-for k, v in jogador.items():
-    print(f'O campo {k} tem o valor {v}')
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
 print()
-print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas!')
-for i, v, in enumerate(jogador['gols']):
-    print(f'   => Na partida {i+1}, fez {v} gols.')
-print()
-print(f'Foi um total de {jogador["total"]} gols.')
+while True:
+    busca = int(input('Mostrar dados de qual jogador?: (999) para cancelar'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'Erro! Codigo {busca} não existe')
+    else:
+        print(f'Detalhes do jogador {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols')
+    print()
+print('FIM')
