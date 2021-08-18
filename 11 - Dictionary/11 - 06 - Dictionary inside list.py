ca0fe189ac624c94373b8d20477572eb4784cbfc
Denis.Husch <denis.husch@gmail.com>
2021-08-17 22:12:22 -0300

    Exerc 5

--- 11 - Dictionary/11 - 06 - Dictionary inside list.py
+++ 11 - Dictionary/11 - 06 - Dictionary inside list.py
@@ -4,6 +4,7 @@
 #Shows how many people recorded, age avarage, a woman list and shows the age above the avarage
 galera = list()
 pessoa = dict()
+soma = media = 0
 while True:
     pessoa.clear()
     pessoa['nome'] = str(input('Nome: '))
@@ -13,6 +14,7 @@ while True:
             break
         print('Escolha M ou F')
     pessoa['idade'] = int(input('Idade: '))
+    soma = soma + pessoa['idade']
     galera.append(pessoa.copy())
     while True:
         resp = str(input('Quer continuar? [S/N]: ')).upper()[0]
@@ -20,4 +22,20 @@ while True:
             break
     if resp == 'N':
         break
+media = soma / len(galera)
 print(galera)
+print(f'Pessoas cadastradas: {len(galera)}')
+print(f'A media de idade é {media:5.2f} anos')
+print(f'As mulheres foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}', end='')
+print()
+print('As pessoas acima da media:')
+for p in galera:
+    if p['idade'] >= media:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('FIM')
