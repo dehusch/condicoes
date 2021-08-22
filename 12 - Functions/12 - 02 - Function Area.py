5b7f651b035cb5d12a9ae97cdc41bb98cc63e88c
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-21 22:47:37 -0300

    Exerc 01 - 02

---
+++ 12 - Functions/12 - 02 - Function Area.py
@@ -0,0 +1,9 @@
+#create a square calc terrain with function
+def area():
+    area = largura * comprimento
+    print(f'A area do terreno {largura}x{comprimento} é de {area}')
+
+
+#Programa principal
+largura = float(input('LARGURA: '))
+comprimento = float(input('COMPRIMENTO: '))
