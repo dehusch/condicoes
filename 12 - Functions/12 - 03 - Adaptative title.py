71b69df175ad2716496794a82e2ae8b83536a24b
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-21 23:21:32 -0300

    update

---
+++ 12 - Functions/12 - 03 - Adaptative title.py
@@ -0,0 +1,12 @@
+#Create lines according title lenght.
+#---------
+#  TITLE
+#---------
+#frase.count()
+#len(frase)
+def titulo(txt):
+    cont = len(titulo)
+    print(cont)
+
+
+titulo('CONTAGEM DE LETRAS')
