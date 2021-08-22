29f4e3a6da28cae10ee61667ee9a0a172c171111
dehusch <denis.husch@gmail.com>
2021-08-22 19:27:53 -0300

    Finish exerc 4

--- 12 - Functions/12 - 04 - Counting DEF's.py
+++ 12 - Functions/12 - 04 - Counting DEF's.py
@@ -3,19 +3,23 @@
 #Counting to unknown number by unknown steps
 from time import sleep
 def contador(i, f, p): #INICIO, FIM, PASSO
+    if p < 0:   #aqui é que define o passo personalizado
+        p = p * -1
+    if p ==0:
+        p = 1
     print(f'Contagem de {i}, até {f} de {p} em {p}')
     if i < f:
         cont = i
         while cont <= f:
             print(f'{cont} ', end='', flush=True)
-            sleep(0.5)
+            sleep(0.2)
             cont += p
         print('Fim!')
     else:
         cont = i
         while cont >= f:
             print(f'{cont} ', end='', flush=True)
-            sleep(0.5)
+            sleep(0.2)
             cont -= p
         print('Fim!')
 
@@ -23,8 +27,11 @@ def contador(i, f, p): #INICIO, FIM, PASSO
 #Programa principal
 contador(1, 10, 1)
 contador(10, 0, 2)
-
-
+print()
+ini = int(input('Digite o inicio: '))
+fim = int(input('Digite o fim:'))
+pas = int(input('Digite o passo: '))
+contador(ini, fim, pas)
 
 
 
@@ -42,6 +49,7 @@ contador(10, 0, 2)
 #
 #My way
 #
+print()
 def count10():
     for c in range(0, 10):
         c = c + 1
@@ -59,16 +67,3 @@ def count10by2():
 
 
 print(count10by2())
-
-
-def count():
-    inicio = int(input('Digite o inicio: '))
-    fim = int(input('Digite o fim:'))
-    passo = int(input('Digite o passo: '))
-    for c in range(inicio, fim):
-        c = c * passo
-        print(c)
-    print('Fim')
-
-print(count())
-    
