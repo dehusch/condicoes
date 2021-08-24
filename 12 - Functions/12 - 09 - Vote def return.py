f88013aa2cea7fc0b7214a4eb8acc10ad1f5ed93
dehusch <denis.husch@gmail.com>
2021-08-24 20:52:14 -0300

    Exerc 09

--- 12 - Functions/12 - 09 - Vote def return.py
+++ 12 - Functions/12 - 09 - Vote def return.py
@@ -1,7 +1,34 @@
+#Vote ranged age
+#16 <
+#16 > 18
+#18 > 65
+#65 >
+def voto(year):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - year
+    if idade < 16:
+        return f'Com {idade} anos: Não vota'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: Voto opcional'
+    else:
+        return f'Com {idade} anos: Voto Obrigatorio'
+
+
+birth = int(input('Em que ano você nasceu?: '))
+print(voto(birth))
+#
+#
+#
+#
+#
+#How I thought it was....
+#
+#
 def nasc(n):
     global ano
     ano = 2021 - n
-    if ano <= 18:
+    if ano <= 16:
         return True
     else:
         return False
