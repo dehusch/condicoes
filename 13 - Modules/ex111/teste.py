9616b200ab8e082b70ef1af079918e650b934e44
dehusch <denis.huscu@gmail.com>
2021-08-29 01:12:12 -0300

    Exerc 05

---
+++ 13 - Modules/ex111/teste.py
@@ -0,0 +1,15 @@
+#THE Package in VScode doesn't need a folder especification in import line
+#THIS IS VERY IMPORTANT
+#THE Package in VScode doesn't need a folder especification in import line
+#THIS IS VERY IMPORTANT
+#THE Package in VScode doesn't need a folder especification in import line
+#THIS IS VERY IMPORTANT
+
+from utilidadescev import moeda
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p)
+#UMA ALTERAÇÃO NO PROGRAMA PADRAO
+print()
+print('Com 10% de aumento e 20% de redução')
+print()
+moeda.resumo(p, 10, 20) #10% de aumento e 20% de redução
